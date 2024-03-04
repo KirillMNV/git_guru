44919ce26fe0cc485f170f9750c1d01fa7cea965
Manov Kirill <manovkirill93@gmail.com>
2024-03-04 15:37:47 +0600

    Добавлен sout

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 public class NewTest {
     @Test
     void newTest(){
+        System.out.println("Best test eva");
         Assertions.assertTrue(3>2);
     }
 }
