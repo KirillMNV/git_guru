1c54c76c68c13ac2a8a7f46d48552b9750722ca6
Manov Kirill <manovkirill93@gmail.com>
2024-03-04 15:02:48 +0600

    Добавлен новый тест

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class NewTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NewTest {
+    @Test
+    void newTest(){
+        Assertions.assertTrue(3>2);
+    }
 }
