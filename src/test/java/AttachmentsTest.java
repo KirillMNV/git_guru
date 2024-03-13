8d6555898816fe08545fea92444eb00cff038011
Manov Kirill <manovkirill93@gmail.com>
2024-03-13 22:21:50 +0600

    "Добавлен тест со степами"

--- src/test/java/AttachmentsTest.java
+++ src/test/java/AttachmentsTest.java
@@ -1,26 +1,22 @@
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.attachment;
 import static io.qameta.allure.Allure.step;
 
 
-public class NewTest {
-    @Test
-    void newTest() {
-        System.out.println("Best test eva");
-        Assertions.assertTrue(3 > 2);
-    }
+public class AttachmentsTest {
 
     @Test
-    public void testIssue() {
+    public void AttachmentstestIssue() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
         step("Открываем главную страницу Github", () -> {
             open("https://github.com");
+            attachment("Sourse",webdriver().driver().source());
         });
         step("Нажимаем на поисковую строку", () -> {
             $("[data-target='qbsearch-input.inputButtonText']").click();
--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,10 +1,35 @@
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+
 public class NewTest {
     @Test
-    void newTest(){
+    void newTest() {
         System.out.println("Best test eva");
-        Assertions.assertTrue(3>2);
+        Assertions.assertTrue(3 > 2);
+    }
+
+    @Test
+    public void testIssue() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу Github", () -> {
+            open("https://github.com");
+        });
+        step("Нажимаем на поисковую строку", () -> {
+            $("[data-target='qbsearch-input.inputButtonText']").click();
+        });
+        step("Вписываем нужную инфу и поддтверждаем", () -> {
+            $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
+            $("#query-builder-test").pressEnter();
+        });
+
+
     }
 }
